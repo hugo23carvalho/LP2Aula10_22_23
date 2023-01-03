dbb6606583062509024c8ace262df81d7ca983a5
hugo23carvalho <45176065+hugo23carvalho@users.noreply.github.com>
2023-01-03 10:31:17 +0000

    Add Threads folder + Exercise 1 Done

---
+++ FrogRace/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace FrogRace
+{
+    class Program
+    {
+         private static void Main(string[] args)
+        {
+            Thread t1 = new Thread (FrogRace);
+            Thread t2 = new Thread (FrogRace);
+            Thread t3 = new Thread (FrogRace);
+
+            t1.Name = "T_One";
+            t2.Name = "T_Two";
+            t3.Name = "T_Three";
+            t1.Start(1);
+            t2.Start(2);
+            t3.Start(3);
+            t1.Join();
+            t2.Join();
+            t3.Join();
+        }
+
+        public static void FrogRace (object obj)
+        {
+            int FrogNumb = (int)obj;
+            
+            string name = Thread.CurrentThread.Name;
+
+            Random rnd = new Random(FrogNumb);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(rnd.Next(1001));
+                Console.WriteLine($"Rã {FrogNumb} deu o salto {i + 1} (Thread {name})");
+            }
+
+            Console.WriteLine($"Rã{FrogNumb} acabou!");
+        }
+    }
+}
