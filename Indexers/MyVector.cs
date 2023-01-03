50aa4ba803a4767bbd6facd43eb9c2bc30679244
hugo23carvalho <45176065+hugo23carvalho@users.noreply.github.com>
2023-01-03 12:08:22 +0000

    Exercise 3 Done

---
+++ Indexers/MyVector.cs
@@ -0,0 +1,65 @@
+﻿namespace Indexers
+{
+    public struct MyVector
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public MyVector(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public float this [int index]
+        {
+            get
+            {
+                if (index == 0) { return X;}
+                else if (index ==1) {return Y;}
+                else { throw new IndexOutOfRangeException();}
+            }
+
+            set
+            {
+                if (index == 0) X = value;
+                else if (index == 1) Y = value;
+                else throw new IndexOutOfRangeException();
+            }
+        }
+
+        private static string indx0 = "xa0";
+        private static string indx1 = "yb1";
+
+        public float this[string index]
+        {
+            get
+            {
+              index = index?.ToLower() ?? "";
+
+              if(index.Length == 1)
+              {
+                if (indx0.Contains(index))
+                    return X;
+                else if (indx1.Contains(index))
+                    return Y;
+              } 
+              throw new IndexOutOfRangeException();
+            }
+
+            set
+            {
+               index = index?.ToLower() ?? "";
+
+              if(index.Length == 1)
+              {
+                if (indx0.Contains(index))
+                    X = value;
+                else if (indx1.Contains(index))
+                    Y = value;
+                else throw new IndexOutOfRangeException();
+              } 
+              else throw new IndexOutOfRangeException(); 
+            }
+        }
+    }
+}
